0cb8d4fd30c0833acfb5b033ca1ac61a1365a5c7
Dead End <dead.end@inbox.ru>
2025-03-28 23:25:46 +0100

    added definitions

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import './assets/reset.css';
 import './assets/style.css';
+import { PageLayout } from './components/PageLayout';
+import { IndexPage } from './pages/IndexPage';
 import { Table } from './pages/table';
 
 console.log('Started');
 
+customElements.define('page-layout', PageLayout);
 customElements.define('my-table', Table);
+customElements.define('index-page', IndexPage);
