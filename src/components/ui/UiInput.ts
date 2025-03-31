ac9ab53de3210946f57525709b15a4db54fc2c31
Dead End <dead.end@inbox.ru>
2025-03-31 21:33:07 +0200

    added ui input

---
+++ src/components/ui/UiInput.ts
@@ -0,0 +1,32 @@
+import { $, tmplClone } from '../../lib/utils';
+
+export class UiInput extends HTMLElement {
+  static TMPL = $<HTMLTemplateElement>('#ui-input');
+  initialized = false;
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    if (!this.initialized) {
+      const tmpl = tmplClone(UiInput.TMPL);
+
+      const labelElem = $<HTMLLabelElement>('label', tmpl);
+      const inputElem = $<HTMLInputElement>('input', tmpl);
+      const errElem = $<HTMLParagraphElement>('p', tmpl);
+
+      const id = this.getAttribute('data-id') || 'no-id';
+
+      labelElem.htmlFor = id;
+      inputElem.id = id;
+      errElem.setAttribute('data-for', id);
+
+      labelElem.textContent = this.getAttribute('data-label') || 'no-label';
+      inputElem.type = this.getAttribute('data-type') || 'text';
+
+      this.appendChild(tmpl);
+      this.initialized = true;
+    }
+  }
+}
