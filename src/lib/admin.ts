b5ab66b852aa3784f9ffeaf52084a4ef79a19abb
Dead End <dead.end@inbox.ru>
2025-04-04 23:43:40 +0200

    using navigation

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -2,6 +2,11 @@ import { TAdmin } from './types';
 
 const STORE_KEY = 'admin';
 
+export const adminInit = () => {
+  const event = adminIsLogin() ? new Event('login') : new Event('logout');
+  document.dispatchEvent(event);
+};
+
 export const adminIsLogin = () => {
   return localStorage.getItem(STORE_KEY) !== null;
 };
@@ -12,10 +17,12 @@ export const adminLogin = (url: string, token: string) => {
     token,
   };
   localStorage.setItem(STORE_KEY, JSON.stringify(admin));
+  document.dispatchEvent(new Event('login'));
 };
 
 export const adminLogout = () => {
   if (localStorage.getItem(STORE_KEY) !== null) {
     localStorage.removeItem(STORE_KEY);
+    document.dispatchEvent(new Event('logout'));
   }
 };
