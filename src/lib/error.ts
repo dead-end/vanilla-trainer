1a139df66b18e28d631862f0c481d1872ea2c88f
Dead End <dead.end@inbox.ru>
2025-04-06 11:01:12 +0200

    update error

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -2,19 +2,20 @@ import { $, $$ } from './utils';
 
 export const errorReset = (root: HTMLElement) => {
   $$('p[data-error]', root).forEach((p) => {
-    p.hidden = true;
+    p.style.display = 'none';
     p.textContent = '';
   });
 };
 
 export const errorSet = (root: HTMLElement, name: string, msg: string) => {
   const p = $(`p[data-error="${name}"]`, root);
-  p.hidden = false;
+  p.style.display = 'block';
   p.textContent = msg;
 };
 
 export const errorExists = (root: HTMLElement) => {
   return (
-    $$('p[data-error]', root).find((p) => p.hidden === false) !== undefined
+    $$('p[data-error]', root).find((p) => p.style.display !== 'none') !==
+    undefined
   );
 };
