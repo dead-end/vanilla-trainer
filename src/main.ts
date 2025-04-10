82ccdebe56dcb0e6a62841dc50072d66bb7ee8f5
Dead End <dead.end@inbox.ru>
2025-04-10 22:41:16 +0200

    added book list

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { UiInput } from './components/ui/UiInput';
 import { adminInit } from './lib/admin';
 import { routeInit, routeRegister } from './lib/route';
 import { AdminPage } from './pages/AdminPage';
+import { BookListPage } from './pages/book/BookListPage';
 import { IndexPage } from './pages/IndexPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 
@@ -13,6 +14,7 @@ console.log('Started');
 
 routeRegister('#/', 'index-page');
 routeRegister('#/admin', 'admin-page');
+routeRegister('#/books', 'book-list-page');
 //routeRegister('*', 'not-found-page');
 
 customElements.define('navi-gation', Navigation);
@@ -23,6 +25,7 @@ customElements.define('ui-icons', Icons);
 customElements.define('not-found-page', NotFoundPage);
 customElements.define('index-page', IndexPage);
 customElements.define('admin-page', AdminPage);
+customElements.define('book-list-page', BookListPage);
 
 routeInit();
 
