2f3f599e388d09bd274f5c320699118293dffb68
Dead End <dead.end@inbox.ru>
2025-04-11 10:43:23 +0200

    local vars

--- src/pages/AdminPage.ts
+++ src/pages/AdminPage.ts
@@ -5,27 +5,27 @@ import { $, tmplClone } from '../lib/utils';
 export class AdminPage extends HTMLElement {
   static TMPL = $<HTMLTemplateElement>('#page-admin');
 
-  form: HTMLFormElement | undefined;
-  user: HTMLInputElement | undefined;
-  repo: HTMLInputElement | undefined;
-  token: HTMLInputElement | undefined;
+  _form: HTMLFormElement | undefined;
+  _user: HTMLInputElement | undefined;
+  _repo: HTMLInputElement | undefined;
+  _token: HTMLInputElement | undefined;
 
   constructor() {
     super();
   }
 
   connectedCallback() {
-    if (!this.form) {
+    if (!this._form) {
       const tmpl = tmplClone(AdminPage.TMPL);
 
-      this.form = $<HTMLFormElement>('form', tmpl);
-      this.form.onsubmit = this.handleSubmit.bind(this);
+      this._form = $<HTMLFormElement>('form', tmpl);
+      this._form.onsubmit = this.handleSubmit.bind(this);
 
       this.appendChild(tmpl);
 
-      this.user = $<HTMLInputElement>('#user', this.form);
-      this.repo = $<HTMLInputElement>('#repo', this.form);
-      this.token = $<HTMLInputElement>('#token', this.form);
+      this._user = $<HTMLInputElement>('#user', this._form);
+      this._repo = $<HTMLInputElement>('#repo', this._form);
+      this._token = $<HTMLInputElement>('#token', this._form);
 
       document.addEventListener('logout', this.onLogout.bind(this));
 
@@ -36,36 +36,36 @@ export class AdminPage extends HTMLElement {
   handleSubmit(e: SubmitEvent) {
     e.preventDefault();
 
-    if (!this.user || !this.repo || !this.token) {
+    if (!this._user || !this._repo || !this._token) {
       throw new Error('Not initialized!');
     }
 
     errorReset(this);
 
-    if (!this.user.value) {
+    if (!this._user.value) {
       errorSet(this, 'user', 'Please enter a value!');
     }
-    if (!this.repo.value) {
+    if (!this._repo.value) {
       errorSet(this, 'repo', 'Please enter a value!');
     }
-    if (!this.token.value) {
+    if (!this._token.value) {
       errorSet(this, 'token', 'Please enter a value!');
     }
 
     if (!errorExists(this)) {
-      adminLogin(this.user.value, this.repo.value, this.token.value);
+      adminLogin(this._user.value, this._repo.value, this._token.value);
     }
   }
 
   async getAdmin() {
-    if (!this.user || !this.repo || !this.token) {
+    if (!this._user || !this._repo || !this._token) {
       throw new Error('Not initialized!');
     }
 
     const admin = await adminGet();
-    this.user.value = admin.user;
-    this.repo.value = admin.repo;
-    this.token.value = admin.token;
+    this._user.value = admin.user;
+    this._repo.value = admin.repo;
+    this._token.value = admin.token;
   }
 
   onLogout() {
