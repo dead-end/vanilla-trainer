10c8f688b395dcbb7d286d5838e07993b18aa321
Dead End <dead.end@inbox.ru>
2025-04-13 09:50:45 +0200

    update route with regex

--- src/lib/route.ts
+++ src/lib/route.ts
@@ -1,36 +1,69 @@
 import { adminIsLogin } from './admin';
+import { errorGlobal } from './error';
 import { TNav } from './types';
 import { $ } from './utils';
 
-const notFound: TNav = {
-  hash: '*',
-  page: 'not-found-page',
+const routes: TNav[] = [];
+let routeParams: RegExpExecArray | null;
+let indexHash: string;
+let adminHash: string;
+
+/**
+ * The function returns a route parameter with a given name. If creates an
+ * error if the parameter does not exist.
+ */
+export const getRouteParam = (name: string) => {
+  if (!routeParams) {
+    errorGlobal(
+      `Route parameter: ${name} - No parameters found: ${window.location.hash}`
+    );
+    return null;
+  }
+  if (!routeParams.groups) {
+    errorGlobal(
+      `Route parameter: ${name} - This parameter not found: ${window.location.hash}`
+    );
+    return null;
+  }
+
+  return routeParams.groups[name];
 };
 
-const routes: TNav[] = [];
-
+/**
+ * The function handles has changes. It only works if we have not found page
+ * with an appropriate regex at the end of the nav list.
+ */
 const handleHashChange = async () => {
-  let hash = window.location.hash || '#/';
+  let hash = window.location.hash || indexHash;
 
-  if (!(await adminIsLogin()) && hash !== '#/admin') {
-    window.location.hash = '#/admin';
+  if (!(await adminIsLogin()) && hash !== adminHash) {
+    window.location.hash = adminHash;
     return;
   }
 
-  const nav = routes.find((n) => n.hash === hash) || notFound;
+  const nav = routes.find((n) => n.regex.test(window.location.hash)) as TNav;
+
+  routeParams = nav.regex.exec(window.location.hash);
+
   const element = document.createElement(nav.page);
   $('main').replaceChildren(element);
 };
 
-export const routesGet = () => {
-  return routes;
+/**
+ * Register a new route.
+ *
+ * TODO: Create a regex from a string: escape
+ */
+export const routeRegister = (regex: RegExp, page: string) => {
+  routes.push({ regex, page });
 };
 
-export const routeRegister = (hash: string, page: string) => {
-  routes.push({ hash, page });
-};
-
-export const routeInit = () => {
+/**
+ * Initialize the routes with the index and admin hash.
+ */
+export const routeInit = (idxHash: string, admhash: string) => {
+  indexHash = idxHash;
+  adminHash = admhash;
   window.addEventListener('hashchange', handleHashChange);
   window.addEventListener('DOMContentLoaded', handleHashChange);
 };
