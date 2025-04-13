42aa3f60a808452b0ce555b793e2a7c5a267dbbb
Dead End <dead.end@inbox.ru>
2025-04-13 09:51:18 +0200

    added book update with regex

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,16 @@ import { BookListPage } from './pages/book/BookListPage';
 import { BookCreatePage } from './pages/book/BookCreatePage';
 import { UiTextarea } from './components/ui/UiTextarea';
 import { ErrorMsg } from './components/ErrorMsg';
+import { BookUpdatePage } from './pages/book/BookUpdatePage';
 
 console.log('Started');
 
-routeRegister('#/', 'index-page');
-routeRegister('#/admin', 'admin-page');
-routeRegister('#/books', 'book-list-page');
-routeRegister('#/books/create', 'book-create-page');
-//routeRegister('*', 'not-found-page');
+routeRegister(/^#\/$/, 'index-page');
+routeRegister(/^#\/admin$/, 'admin-page');
+routeRegister(/^#\/books$/, 'book-list-page');
+routeRegister(/^#\/books\/create$/, 'book-create-page');
+routeRegister(/^#\/book\/update\/(?<bookId>[^\/]+)$/, 'book-update-page');
+routeRegister(/.*/, 'not-found-page');
 
 customElements.define('navi-gation', Navigation);
 customElements.define('page-header', PageHeader);
@@ -35,7 +37,8 @@ customElements.define('index-page', IndexPage);
 customElements.define('admin-page', AdminPage);
 customElements.define('book-list-page', BookListPage);
 customElements.define('book-create-page', BookCreatePage);
+customElements.define('book-update-page', BookUpdatePage);
 
-routeInit();
+routeInit('#/', '#/admin');
 
 adminInit();
