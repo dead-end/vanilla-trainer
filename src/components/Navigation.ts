38a65c5b76e2a9519b7f94553714df40bfe1c46d
Dead End <dead.end@inbox.ru>
2025-04-18 11:01:07 +0200

    nav with shadow tree and styles

--- src/components/Navigation.ts
+++ src/components/Navigation.ts
@@ -1,24 +1,25 @@
 import { adminLogout } from '../lib/admin';
+import { STYLES } from '../lib/ui/stylesheets';
 import { $, tmplClone } from '../lib/utils';
 
 export class Navigation extends HTMLElement {
   static TMPL = $<HTMLTemplateElement>('#tmpl-navigation');
-  navItems: HTMLElement | undefined;
+  navItems: HTMLElement;
 
   constructor() {
     super();
-  }
 
-  connectedCallback() {
-    if (!this.navItems) {
-      const tmpl = tmplClone(Navigation.TMPL);
-      this.navItems = $<HTMLElement>('#nav-items', tmpl);
-      $('#logout', tmpl).onclick = this.doLogout;
-      this.appendChild(tmpl);
+    this.attachShadow({ mode: 'open' }).adoptedStyleSheets = STYLES;
 
-      document.addEventListener('login', this.onLogin.bind(this));
-      document.addEventListener('logout', this.onLogout.bind(this));
-    }
+    const tmpl = tmplClone(Navigation.TMPL);
+
+    this.navItems = $<HTMLElement>('#nav-items', tmpl);
+    $('#logout', tmpl).onclick = this.doLogout;
+
+    this.shadowRoot?.appendChild(tmpl);
+
+    document.addEventListener('login', this.onLogin.bind(this));
+    document.addEventListener('logout', this.onLogout.bind(this));
   }
 
   onLogin() {
