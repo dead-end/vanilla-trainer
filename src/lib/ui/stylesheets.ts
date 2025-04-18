666b46947bfbc4cf80f228dedc861531abfeff6d
Dead End <dead.end@inbox.ru>
2025-04-18 09:54:43 +0200

    correct path

--- src/lib/ui/stylesheets.ts
+++ src/lib/ui/stylesheets.ts
@@ -1,5 +1,5 @@
-import reset from '../assets/reset.css?raw';
-import style from '../assets/style.css?raw';
+import reset from '../../assets/reset.css?raw';
+import style from '../../assets/style.css?raw';
 
 const cssReset = new CSSStyleSheet();
 cssReset.replace(reset);
