c68c35500a0f768888c943f06cc32fe891ee21b2
Dead End <dead.end@inbox.ru>
2025-04-24 21:12:18 +0200

    create chapter

---
+++ src/pages/chapter/ChapterCreatePage.ts
@@ -0,0 +1,73 @@
+import { fieldErrorExists, fieldErrorReset } from '../../lib/ui/field';
+import { githubConfigGet } from '../../lib/model/githubConfig';
+import { $, errorGlobal, tmplClone } from '../../lib/utils';
+import { fieldGet, fieldId, fieldRequired } from '../../lib/ui/field';
+import { chapterCreate } from '../../lib/model/chapter';
+import { getRouteParam } from '../../lib/route';
+import { hashChapterList } from '../../lib/hash';
+
+export class ChapterCreatePage extends HTMLElement {
+  static TMPL = $<HTMLTemplateElement>('#chapter-create-page');
+
+  connectedCallback() {
+    if (!this.hasChildNodes()) {
+      const tmpl = tmplClone(ChapterCreatePage.TMPL);
+      $<HTMLFormElement>('form', tmpl).onsubmit = this.handleSubmit.bind(this);
+      this.appendChild(tmpl);
+    }
+
+    this.render();
+  }
+
+  render() {
+    const bookId = getRouteParam('bookId');
+    const link = hashChapterList(bookId);
+    $<HTMLAnchorElement>('#chapter-list-link', this).href = link;
+  }
+
+  async handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+
+    const bookId = getRouteParam('bookId');
+
+    const form = e.target as HTMLFormElement;
+
+    const formData = new FormData(form);
+    const id = fieldGet(formData, 'id');
+    const title = fieldGet(formData, 'title');
+
+    fieldErrorReset(form);
+
+    fieldRequired(form, id) && fieldId(form, id);
+    fieldRequired(form, title);
+
+    if (!fieldErrorExists(form)) {
+      const button = $<HTMLButtonElement>('#btn-submit', form);
+      button.disabled = true;
+
+      this.doCreate(bookId, id.value, title.value).finally(() => {
+        button.disabled = false;
+      });
+    }
+  }
+
+  async doCreate(bookId: string, id: string, title: string) {
+    const githubConfig = await githubConfigGet();
+    if (!githubConfig) {
+      errorGlobal('Unable to get github config!');
+      return;
+    }
+
+    const result = await chapterCreate(githubConfig, bookId, {
+      id,
+      title,
+    });
+
+    if (result.hasError()) {
+      errorGlobal(`Unable to create a new chapter: ${result.getMessage()}`);
+      return;
+    }
+
+    window.location.hash = hashChapterList(bookId);
+  }
+}
