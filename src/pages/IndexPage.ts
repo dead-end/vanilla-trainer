739ceb0ec2ec8212332ce9cf17889bffd34174c9
Dead End <dead.end@inbox.ru>
2025-05-09 23:22:28 +0200

    Added continue and end

--- src/pages/IndexPage.ts
+++ src/pages/IndexPage.ts
@@ -1,3 +1,5 @@
+import { hashLessionProcess } from '../lib/hash';
+import { lessionExists, lessionRemove } from '../lib/model/lession';
 import { $, tmplClone } from '../lib/utils';
 
 export class IndexPage extends HTMLElement {
@@ -6,7 +8,25 @@ export class IndexPage extends HTMLElement {
   connectedCallback() {
     if (!this.hasChildNodes()) {
       const tmpl = tmplClone(IndexPage.TMPL);
+
+      if (lessionExists()) {
+        $<HTMLButtonElement>('#btn-continue', tmpl).onclick =
+          this.onContinue.bind(this);
+        $<HTMLButtonElement>('#btn-end', tmpl).onclick = this.onEnd.bind(this);
+      } else {
+        $<HTMLElement>('#lession', tmpl).style.display = 'none';
+      }
+
       this.appendChild(tmpl);
     }
+  }
+
+  onContinue() {
+    window.location.hash = hashLessionProcess();
+  }
+
+  onEnd() {
+    lessionRemove();
+    $<HTMLElement>('#lession').style.display = 'none';
   }
 }
