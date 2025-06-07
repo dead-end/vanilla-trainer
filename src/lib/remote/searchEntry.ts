166199d698678459351dea63de8f37a06bcd7494
Dead End <dead.end@inbox.ru>
2025-06-07 09:26:13 +0200

    added file

---
+++ src/lib/remote/searchEntry.ts
@@ -0,0 +1,50 @@
+import {
+  storeDel,
+  storeGet,
+  storeGetAll,
+  storePut,
+  storeTx,
+} from '../persist/store';
+import { TSearch } from '../types';
+
+// TODO: combine searchEntry and cacheEntry
+// Both are nearly identical. We can share a transaction for both.
+
+const STORE = 'search';
+
+export const searchEntryList = async () => {
+  const storeRead = await storeTx(STORE, 'readonly');
+  return await storeGetAll<TSearch>(storeRead);
+};
+
+/**
+ * The function deletes the cache entry.
+ */
+export const searchEntryDelete = async (path: string) => {
+  const store = await storeTx(STORE, 'readwrite');
+  await storeDel(store, path);
+};
+
+/**
+ * The function creates or updates a cache entry.
+ */
+export const searchEntryPut = async (
+  path: string,
+  strs: string[],
+  hash: string
+) => {
+  const storeWrite = await storeTx(STORE, 'readwrite');
+  await storePut<TSearch>(storeWrite, {
+    path,
+    strs,
+    hash,
+  });
+};
+
+/**
+ * The function returns a cache entry or undefined.
+ */
+export const searchEntryGet = async (path: string) => {
+  const storeRead = await storeTx(STORE, 'readonly');
+  return await storeGet<TSearch>(storeRead, path);
+};
