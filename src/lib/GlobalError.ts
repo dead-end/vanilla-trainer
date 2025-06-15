6e308419956beccdd8e5617b0752d70008bada2f
Dead End <dead.end@inbox.ru>
2025-06-15 22:48:48 +0200

    added todo

--- src/lib/GlobalError.ts
+++ src/lib/GlobalError.ts
@@ -1,3 +1,4 @@
+// TODO: naming of the function / class : dispatchError
 export const errorGlobal = (msg: string) => {
   const event = new CustomEvent('error-msg', { detail: msg });
   document.dispatchEvent(event);
