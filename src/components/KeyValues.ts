7a9fd21eb893fa61cc1bcf3c79759bd8c702787e
Dead End <dead.end@inbox.ru>
2025-06-22 11:20:49 +0200

    removed table from key-values

--- src/components/KeyValues.ts
+++ src/components/KeyValues.ts
@@ -3,15 +3,12 @@ import { $ } from '../lib/utils/query';
 import { tmplClone } from '../lib/utils/tmpl';
 
 export class KeyValues extends HTMLElement {
-  static TMPL_LINE = $<HTMLTemplateElement>('#tmpl-key-values-line');
+  static TMPL = $<HTMLTemplateElement>('#tmpl-key-values');
   static TMPL_ENTRY = $<HTMLTemplateElement>('#tmpl-key-values-entry');
-
-  static TMPL_TABLE = $<HTMLTemplateElement>('#tmpl-key-values-table');
-  static TMPL_ROW = $<HTMLTemplateElement>('#tmpl-key-values-row');
 
   connectedCallback() {
     if (!this.shadowRoot) {
-      const tmpl = this.getWrapperTmpl();
+      const tmpl = tmplClone(KeyValues.TMPL);
 
       const shadow = this.attachShadow({ mode: 'open' });
       shadow.adoptedStyleSheets = STYLES;
@@ -24,27 +21,13 @@ export class KeyValues extends HTMLElement {
       const arr: DocumentFragment[] = [];
 
       data.forEach((d) => {
-        const tmpl = this.getDataTmpl();
-        $<HTMLElement>('[data-id="key"]', tmpl).textContent = `${d.key}:`;
+        const tmpl = tmplClone(KeyValues.TMPL_ENTRY);
+        $<HTMLElement>('[data-id="key"]', tmpl).textContent = d.key;
         $<HTMLElement>('[data-id="value"]', tmpl).textContent = d.value;
         arr.push(tmpl);
       });
 
       $<HTMLElement>('#wrapper', this.shadowRoot).replaceChildren(...arr);
     }
-  }
-
-  getWrapperTmpl() {
-    if (this.getAttribute('type') === 'line') {
-      return tmplClone(KeyValues.TMPL_LINE);
-    }
-    return tmplClone(KeyValues.TMPL_TABLE);
-  }
-
-  getDataTmpl() {
-    if (this.getAttribute('type') === 'line') {
-      return tmplClone(KeyValues.TMPL_ENTRY);
-    }
-    return tmplClone(KeyValues.TMPL_ROW);
   }
 }
