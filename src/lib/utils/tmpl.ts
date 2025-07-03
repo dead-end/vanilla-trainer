19c7d31fcded737d92adc50e67fa2e668892655d
Dead End <dead.end@inbox.ru>
2025-07-03 22:49:13 +0200

    removed unnecessary function

--- src/lib/utils/tmpl.ts
+++ src/lib/utils/tmpl.ts
@@ -4,10 +4,3 @@
 export const tmplClone = (template: HTMLTemplateElement) => {
   return template.content.cloneNode(true) as DocumentFragment;
 };
-
-// TODO: Not used ??
-export const tmplCreate = (str: string) => {
-  const tmpl = document.createElement('template');
-  tmpl.innerHTML = str;
-  return tmpl;
-};
