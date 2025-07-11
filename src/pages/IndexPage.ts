8535d901b3c1e3568fac76166b27e30e75d7d2ff
Dead End <dead.end@inbox.ru>
2025-07-11 23:02:25 +0200

    registration od lession component

--- src/pages/IndexPage.ts
+++ src/pages/IndexPage.ts
@@ -1,5 +1,3 @@
-import { hashLessionProcess } from '../lib/location/hash';
-import { lessionExists, lessionRemove } from '../lib/model/lession';
 import { $ } from '../lib/utils/query';
 import { tmplClone } from '../lib/utils/tmpl';
 
@@ -10,25 +8,7 @@ export class IndexPage extends HTMLElement {
     if (!this.hasChildNodes()) {
       const tmpl = tmplClone(IndexPage.TMPL);
 
-      // TODO: maybe better in navigation
-      if (lessionExists()) {
-        $<HTMLButtonElement>('#btn-continue', tmpl).onclick =
-          this.onContinue.bind(this);
-        $<HTMLButtonElement>('#btn-end', tmpl).onclick = this.onEnd.bind(this);
-      } else {
-        $<HTMLElement>('#lession', tmpl).style.display = 'none';
-      }
-
       this.appendChild(tmpl);
     }
-  }
-
-  onContinue() {
-    window.location.hash = hashLessionProcess();
-  }
-
-  onEnd() {
-    lessionRemove();
-    $<HTMLElement>('#lession').style.display = 'none';
   }
 }
