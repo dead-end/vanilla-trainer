f77f2efa6c08fafd138039866b3085e298059701
Dead End <dead.end@inbox.ru>
2025-07-26 10:14:46 +0200

    using template literals for books

--- src/pages/book/BookListPage.ts
+++ src/pages/book/BookListPage.ts
@@ -1,50 +1,27 @@
 import { ConfirmDialog } from '../../components/ConfirmDialog';
 import { hashBookUpdate, hashChapterList } from '../../lib/location/hash';
 import { bookDelete, bookListing } from '../../lib/model/book';
+import { TBook } from '../../lib/types';
+import { html } from '../../lib/html/html';
 import { $ } from '../../lib/utils/query';
-import { tmplClone } from '../../lib/utils/tmpl';
+import { createFragment } from '../../lib/html/createFragment';
 
 export class BookListPage extends HTMLElement {
-  static TMPL = $<HTMLTemplateElement>('#book-list-page');
-  static TMPL_ROW = $<HTMLTemplateElement>('#tmpl-book-list');
-
   connectedCallback() {
     if (!this.hasChildNodes()) {
-      const tmpl = tmplClone(BookListPage.TMPL);
-      this.appendChild(tmpl);
+      this.appendChild(this.renderPage());
     }
-
     this.render();
   }
 
   async render() {
-    const arr: DocumentFragment[] = [];
     const confirmDialog = $<ConfirmDialog>('#confirm-dialog');
 
-    const books = await bookListing();
-    books.forEach((b) => {
-      const tmpl = tmplClone(BookListPage.TMPL_ROW);
+    const frags = (await bookListing()).map((b) =>
+      this.renderBook(b, confirmDialog)
+    );
 
-      $('[data-id="id"]', tmpl).textContent = b.id;
-      $('[data-id="title"]', tmpl).textContent = b.title;
-      $('[data-id="desc"]', tmpl).textContent = b.description;
-      $<HTMLElement>('[data-icon="delete"]', tmpl).onclick = this.onDelete(
-        confirmDialog,
-        b.id
-      ).bind(this);
-
-      $<HTMLElement>('[data-icon="update"]', tmpl).onclick = () => {
-        window.location.hash = hashBookUpdate(b.id);
-      };
-
-      $<HTMLElement>('[data-icon="list"]', tmpl).onclick = () => {
-        window.location.hash = hashChapterList(b.id);
-      };
-      arr.push(tmpl);
-    });
-
-    $<HTMLElement>('tbody').replaceChildren(...arr);
-    console.log(books);
+    $<HTMLElement>('tbody').replaceChildren(...frags);
   }
 
   onDelete(confirmDialog: ConfirmDialog, bookId: string) {
@@ -62,5 +39,64 @@ export class BookListPage extends HTMLElement {
       await bookDelete(bookId);
       this.render();
     };
+  }
+
+  renderPage() {
+    const str = /* html */ html`
+      <div class="is-column is-gap">
+        <div class="page-title">Book List</div>
+        <table>
+          <thead>
+            <tr>
+              <th class="is-larger-sm">Id</th>
+              <th>Title</th>
+              <th class="is-larger-sm">Description</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+        <div class="is-row is-gap">
+          <a href="#/books/create" class="btn">Create</a>
+          <a href="#/cache/raw/books/books.json" class="btn">Cache</a>
+        </div>
+      </div>
+    `;
+
+    return createFragment(str);
+  }
+
+  renderBook(book: TBook, confirmDialog: ConfirmDialog) {
+    const str = /*html*/ html`
+      <tr>
+        <td class="is-larger-sm">${book.id}</td>
+        <td>${book.title}</td>
+        <td class="is-larger-sm">${book.description}</td>
+        <td data-id="actions">
+          <div class="is-row is-gap-action">
+            <ui-icons data-icon="delete"></ui-icons>
+            <ui-icons data-icon="update"></ui-icons>
+            <ui-icons data-icon="list"></ui-icons>
+          </div>
+        </td>
+      </tr>
+    `;
+
+    const frac = createFragment(str);
+
+    $<HTMLElement>('[data-icon="delete"]', frac).onclick = this.onDelete(
+      confirmDialog,
+      book.id
+    ).bind(this);
+
+    $<HTMLElement>('[data-icon="update"]', frac).onclick = () => {
+      window.location.hash = hashBookUpdate(book.id);
+    };
+
+    $<HTMLElement>('[data-icon="list"]', frac).onclick = () => {
+      window.location.hash = hashChapterList(book.id);
+    };
+
+    return frac;
   }
 }
