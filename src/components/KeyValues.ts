759bcaf1ac4a7339f6917bcf07dd93ecc9cd4f11
Dead End <dead.end@inbox.ru>
2025-08-02 23:41:05 +0200

    using template string

--- src/components/KeyValues.ts
+++ src/components/KeyValues.ts
@@ -1,34 +1,47 @@
+import { createFragment } from '../lib/html/createFragment';
+import { html } from '../lib/html/html';
 import { TKeyValue } from '../lib/types';
 import { STYLES } from '../lib/ui/stylesheets';
 import { $ } from '../lib/utils/query';
-import { tmplClone } from '../lib/utils/tmpl';
 
 export class KeyValues extends HTMLElement {
-  static TMPL = $<HTMLTemplateElement>('#tmpl-key-values');
-  static TMPL_ENTRY = $<HTMLTemplateElement>('#tmpl-key-values-entry');
-
   connectedCallback() {
     if (!this.shadowRoot) {
-      const tmpl = tmplClone(KeyValues.TMPL);
-
       const shadow = this.attachShadow({ mode: 'open' });
       shadow.adoptedStyleSheets = STYLES;
-      shadow.appendChild(tmpl);
+      shadow.appendChild(this.renderComponent());
     }
   }
 
   update(data: TKeyValue[]) {
     if (this.shadowRoot) {
-      const arr: DocumentFragment[] = [];
-
-      data.forEach((d) => {
-        const tmpl = tmplClone(KeyValues.TMPL_ENTRY);
-        $<HTMLElement>('[data-id="key"]', tmpl).textContent = d.key;
-        $<HTMLElement>('[data-id="value"]', tmpl).textContent = d.value;
-        arr.push(tmpl);
-      });
-
+      const arr = data.map((d) => this.renderData(d));
       $<HTMLElement>('#wrapper', this.shadowRoot).replaceChildren(...arr);
     }
+  }
+
+  renderComponent() {
+    const str = /* html */ html`
+      <style>
+        .wrapper {
+          display: grid;
+          grid-gap: var(--gap-small);
+          grid-template-columns: 1fr 1fr 1fr;
+        }
+      </style>
+      <div id="wrapper" class="wrapper"></div>
+    `;
+
+    return createFragment(str);
+  }
+
+  renderData(data: TKeyValue) {
+    const str = /* html */ html`
+      <div class="is-row is-gap-small">
+        <div class="is-key">${data.key}</div>
+        <div class="is-value">${data.value}</div>
+      </div>
+    `;
+    return createFragment(str);
   }
 }
