deb553cf18e0fa02997ff568495f3a79419c2813
Dead End <dead.end@inbox.ru>
2025-08-02 23:46:50 +0200

    usinf template string

--- src/components/ConfirmDialog.ts
+++ src/components/ConfirmDialog.ts
@@ -1,22 +1,24 @@
+import { createFragment } from '../lib/html/createFragment';
+import { html } from '../lib/html/html';
 import { STYLES } from '../lib/ui/stylesheets';
 import { $ } from '../lib/utils/query';
-import { tmplClone } from '../lib/utils/tmpl';
+//import { tmplClone } from '../lib/utils/tmpl';
 
 export class ConfirmDialog extends HTMLElement {
-  static TMPL = $<HTMLTemplateElement>('#tmpl-confirm-dialog');
+  //static TMPL = $<HTMLTemplateElement>('#tmpl-confirm-dialog');
   fct: (() => Promise<void>) | undefined;
 
   connectedCallback() {
     if (!this.shadowRoot) {
-      const tmpl = tmplClone(ConfirmDialog.TMPL);
+      //  const tmpl = tmplClone(ConfirmDialog.TMPL);
 
       const shadow = this.attachShadow({ mode: 'open' });
       shadow.adoptedStyleSheets = STYLES;
-      shadow.appendChild(tmpl);
-
+      shadow.appendChild(this.renderComponent());
+      /*
       $<HTMLElement>('#btn-cancel', shadow).onclick = this.onCancel.bind(this);
       $<HTMLElement>('#btn-ok', shadow).onclick = this.onOk.bind(this);
-
+*/
       this.style.display = 'none';
     }
   }
@@ -45,5 +47,55 @@ export class ConfirmDialog extends HTMLElement {
       this.style.display = 'block';
       this.fct = fct;
     }
+  }
+
+  renderComponent() {
+    const str = /* html */ html`
+      <style>
+        .popup {
+          position: fixed;
+          z-index: 1;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
+          background-color: rgba(0, 0, 0, 0.4);
+        }
+
+        .popup-content {
+          margin: auto;
+          max-width: 25rem;
+          background: var(--color-bg);
+          color: var(--color-fg);
+          margin-top: 10%;
+          padding: 1.8rem;
+        }
+
+        .title {
+          border-bottom: var(--border-size) solid var(--border-color);
+          padding-bottom: 0.8rem;
+        }
+      </style>
+
+      <div class="popup">
+        <div class="popup-content is-column is-gap is-border">
+          <div id="dialog-title" class="title is-text-bold is-text-large">
+            title
+          </div>
+          <p id="dialog-msg">Message</p>
+          <div class="is-row is-gap">
+            <button id="btn-cancel" class="btn" type="button">Cancel</button>
+            <button id="btn-ok" class="btn" type="button">Ok</button>
+          </div>
+        </div>
+      </div>
+    `;
+
+    const frag = createFragment(str);
+
+    $<HTMLElement>('#btn-cancel', frag).onclick = this.onCancel.bind(this);
+    $<HTMLElement>('#btn-ok', frag).onclick = this.onOk.bind(this);
+
+    return frag;
   }
 }
