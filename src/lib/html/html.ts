25471da57607d9266bfaf228191e2ed08012214b
Dead End <dead.end@inbox.ru>
2025-09-06 10:16:37 +0200

    added unsafeHtml

--- src/lib/html/html.ts
+++ src/lib/html/html.ts
@@ -1,6 +1,24 @@
 import { escapeHtml } from './escapeHtml';
 
 /**
+ * The class marks a string as 'unsafe' and is not exported.
+ */
+class UnsafeHtml {
+  str: string;
+
+  constructor(str: string) {
+    this.str = str;
+  }
+}
+
+/**
+ * Return a string, marked as unsafe.
+ */
+export const unsafeHtml = (str: string) => {
+  return new UnsafeHtml(str);
+};
+
+/**
  * Function for template literals. All parameters are html escaped.
  */
 export const html = (template: TemplateStringsArray, ...params: any[]) => {
@@ -8,10 +26,16 @@ export const html = (template: TemplateStringsArray, ...params: any[]) => {
 
   for (let i = 1; i < template.length; i++) {
     let value = params[i - 1];
-    if (typeof value !== 'string') {
-      value = value.toString();
+    let str: string;
+    if (value instanceof UnsafeHtml) {
+      str = value.str;
+    } else if (typeof value !== 'string') {
+      str = escapeHtml(value.toString());
+    } else {
+      str = escapeHtml(value);
     }
-    result.push(escapeHtml(value));
+
+    result.push(str);
     result.push(template[i]);
   }
 
