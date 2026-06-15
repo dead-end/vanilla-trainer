f5bd792c1765eba9d951f4456842eb306e863bab
Dead End <dead.end@inbox.ru>
2026-06-15 17:45:07 +0200

    replaced html escape

--- src/lib/html/escapeHtml.ts
+++ src/lib/html/escapeHtml.ts
@@ -1,18 +1,6 @@
 /**
  * Function to escape a string which is used in innerHtml.
  */
-const escapeHtml_old = (unsafe: string) => {
-  return unsafe
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#039;');
-};
-
-/**
- * Function to escape a string which is used in innerHtml.
- */
 export const escapeHtml = (() => {
   const div = document.createElement('div');
 
