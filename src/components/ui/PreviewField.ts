0a74b14756115834d7889428c0965cc4493d83a8
Dead End <dead.end@inbox.ru>
2026-08-09 09:13:05 +0200

    update

--- src/components/ui/PreviewField.ts
+++ src/components/ui/PreviewField.ts
@@ -4,37 +4,49 @@ import { mdToHtml } from '../../lib/markdown';
 import { STYLES } from '../../lib/ui/stylesheets';
 import { $ } from '../../lib/utils/query';
 
+const COMP_FRAG = createFragment(/* html */ html`
+  <div class="is-grid-2">
+    <slot></slot>
+    <div
+      id="preview"
+      class="is-border is-shadow is-padding-input is-multiline"
+    ></div>
+  </div>
+`);
+
+/**
+ * The class implements the preview field, which contains the content of a
+ * corresponding input field, where the md is replaced with html.
+ *
+ * The id is the id of the corresponding input field.
+ */
 export class PreviewField extends HTMLElement {
+  id: string;
+
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.adoptedStyleSheets = STYLES;
+    shadow.appendChild(COMP_FRAG.cloneNode(true));
+
+    this.id = this.getAttribute('data-id') || 'no-id';
+  }
+
   connectedCallback() {
-    if (!this.shadowRoot) {
-      const shadow = this.attachShadow({ mode: 'open' });
-      shadow.adoptedStyleSheets = STYLES;
-      shadow.appendChild(this.renderComponent());
+    if (this.shadowRoot !== null) {
+      const input = $<HTMLTextAreaElement>(`#${this.id}`);
+      const preview = $<HTMLElement>('#preview', this.shadowRoot);
+
+      input.oninput = () => {
+        console.log('input');
+        preview.innerHTML = mdToHtml(input.value);
+      };
     }
   }
 
-  renderComponent() {
-    const str = /* html */ html`
-      <div class="is-grid-2">
-        <slot></slot>
-        <div
-          id="preview"
-          class="is-border is-shadow is-padding-input is-multiline"
-        ></div>
-      </div>
-    `;
-
-    const frag = createFragment(str);
-
-    const id = this.getAttribute('data-id') || 'no-id';
-    const input = $<HTMLTextAreaElement>(`#${id}`);
-    const preview = $<HTMLElement>('#preview', frag);
-
-    input.oninput = () => {
-      console.log('input');
-      preview.innerHTML = mdToHtml(input.value);
-    };
-
-    return frag;
+  disconnectedCallback() {
+    if (this.shadowRoot != null) {
+      $<HTMLTextAreaElement>(`#${this.id}`).oninput = null;
+    }
   }
 }
--- src/components/ui/UiField.ts
+++ src/components/ui/UiField.ts
@@ -3,14 +3,37 @@ import { html } from '../../lib/html/html';
 import { STYLES } from '../../lib/ui/stylesheets';
 import { $ } from '../../lib/utils/query';
 
+const COMP_FRAG = createFragment(/* html */ html`
+  <div class="is-column is-gap-small">
+    <label class="is-small is-text-bold" for="default-id">Default Label</label>
+    <slot></slot>
+    <p class="is-error is-small" id="error"></p>
+  </div>
+`);
+
+/**
+ * The class implements a wrapper around an input field. It provides a label.
+ * So it needs the id of the input field and the label text as fixed
+ * attributes.
+ * In case of an validation error it uses a dynamic attribute with the error
+ * message.
+ */
 export class UiField extends HTMLElement {
   static observedAttributes = ['data-error'];
 
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.adoptedStyleSheets = STYLES;
+    shadow.appendChild(COMP_FRAG.cloneNode(true));
+  }
+
   connectedCallback() {
-    if (!this.shadowRoot) {
-      const shadow = this.attachShadow({ mode: 'open' });
-      shadow.adoptedStyleSheets = STYLES;
-      shadow.appendChild(this.renderComponent());
+    if (this.shadowRoot !== null) {
+      const id = this.getAttribute('data-id') || 'no-id';
+      const label = $<HTMLLabelElement>('label', this.shadowRoot);
+      label.htmlFor = id;
+      label.textContent = this.getAttribute('data-label') || 'no-label';
     }
   }
 
@@ -20,26 +43,5 @@ export class UiField extends HTMLElement {
         $<HTMLElement>('#error', this.shadowRoot).textContent = newValue;
       }
     }
-  }
-
-  renderComponent() {
-    const str = /* html */ html`
-      <div class="is-column is-gap-small">
-        <label class="is-small is-text-bold" for="default-id"
-          >Default Label</label
-        >
-        <slot></slot>
-        <p class="is-error is-small" id="error"></p>
-      </div>
-    `;
-
-    const frag = createFragment(str);
-
-    const id = this.getAttribute('data-id') || 'no-id';
-    const label = $<HTMLLabelElement>('label', frag);
-    label.htmlFor = id;
-    label.textContent = this.getAttribute('data-label') || 'no-label';
-
-    return frag;
   }
 }
