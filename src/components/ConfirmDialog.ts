6585a3c61e9364375772b131d0b6147ce68303f3
Dead End <dead.end@inbox.ru>
2026-08-09 09:13:57 +0200

    update

--- src/components/ConfirmDialog.ts
+++ src/components/ConfirmDialog.ts
@@ -3,26 +3,86 @@ import { html } from '../lib/html/html';
 import { STYLES } from '../lib/ui/stylesheets';
 import { $ } from '../lib/utils/query';
 
+const COMP_FRAG = createFragment(/* html */ html`
+  <style>
+    .popup {
+      position: fixed;
+      z-index: 1;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .popup-content {
+      margin: auto;
+      max-width: 25rem;
+      background: var(--color-bg);
+      color: var(--color-fg);
+      margin-top: 10%;
+      padding: 1.8rem;
+    }
+
+    .title {
+      border-bottom: var(--border-size) solid var(--border-color);
+      padding-bottom: 0.8rem;
+    }
+  </style>
+
+  <div class="popup">
+    <div class="popup-content is-column is-gap is-border">
+      <div id="dialog-title" class="title is-text-bold is-text-large">
+        title
+      </div>
+      <p id="dialog-msg">Message</p>
+      <div class="is-row is-gap">
+        <button id="btn-cancel" class="btn" type="button">Cancel</button>
+        <button id="btn-ok" class="btn" type="button">Ok</button>
+      </div>
+    </div>
+  </div>
+`);
+
+/**
+ * The class implements a confirmation dialog. The dialog is shown
+ * programmatically with a call of the activate function, which contains a
+ * function, which is called when the ok button is clicked. The ok button
+ * is disabled while the function is running.
+ */
 export class ConfirmDialog extends HTMLElement {
   fct: (() => Promise<void>) | undefined;
 
-  connectedCallback() {
-    if (!this.shadowRoot) {
-      const shadow = this.attachShadow({ mode: 'open' });
-      shadow.adoptedStyleSheets = STYLES;
-      shadow.appendChild(this.renderComponent());
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.adoptedStyleSheets = STYLES;
+    shadow.appendChild(COMP_FRAG.cloneNode(true));
 
-      this.style.display = 'none';
+    this.style.display = 'none';
+  }
+
+  connectedCallback() {
+    if (this.shadowRoot !== null) {
+      $<HTMLElement>('#btn-cancel', this.shadowRoot).onclick = this.onCancel;
+      $<HTMLElement>('#btn-ok', this.shadowRoot).onclick = this.onOk;
     }
   }
 
-  onCancel() {
+  disconnectedCallback() {
+    if (this.shadowRoot != null) {
+      $<HTMLElement>('#btn-cancel', this.shadowRoot).onclick = null;
+      $<HTMLElement>('#btn-ok', this.shadowRoot).onclick = null;
+    }
+  }
+
+  onCancel = () => {
     if (this.shadowRoot) {
       this.style.display = 'none';
     }
-  }
+  };
 
-  async onOk() {
+  onOk = async () => {
     if (this.shadowRoot && this.fct) {
       const btn = $<HTMLButtonElement>('#btn-ok', this.shadowRoot);
       btn.disabled = true;
@@ -31,7 +91,7 @@ export class ConfirmDialog extends HTMLElement {
         btn.disabled = false;
       });
     }
-  }
+  };
 
   activate(title: string, msg: string, fct: () => Promise<void>) {
     if (this.shadowRoot) {
@@ -40,55 +100,5 @@ export class ConfirmDialog extends HTMLElement {
       this.style.display = 'block';
       this.fct = fct;
     }
-  }
-
-  renderComponent() {
-    const str = /* html */ html`
-      <style>
-        .popup {
-          position: fixed;
-          z-index: 1;
-          left: 0;
-          top: 0;
-          width: 100%;
-          height: 100%;
-          background-color: rgba(0, 0, 0, 0.4);
-        }
-
-        .popup-content {
-          margin: auto;
-          max-width: 25rem;
-          background: var(--color-bg);
-          color: var(--color-fg);
-          margin-top: 10%;
-          padding: 1.8rem;
-        }
-
-        .title {
-          border-bottom: var(--border-size) solid var(--border-color);
-          padding-bottom: 0.8rem;
-        }
-      </style>
-
-      <div class="popup">
-        <div class="popup-content is-column is-gap is-border">
-          <div id="dialog-title" class="title is-text-bold is-text-large">
-            title
-          </div>
-          <p id="dialog-msg">Message</p>
-          <div class="is-row is-gap">
-            <button id="btn-cancel" class="btn" type="button">Cancel</button>
-            <button id="btn-ok" class="btn" type="button">Ok</button>
-          </div>
-        </div>
-      </div>
-    `;
-
-    const frag = createFragment(str);
-
-    $<HTMLElement>('#btn-cancel', frag).onclick = this.onCancel.bind(this);
-    $<HTMLElement>('#btn-ok', frag).onclick = this.onOk.bind(this);
-
-    return frag;
   }
 }
--- src/components/ErrorMsg.ts
+++ src/components/ErrorMsg.ts
@@ -3,47 +3,57 @@ import { html } from '../lib/html/html';
 import { STYLES } from '../lib/ui/stylesheets';
 import { $ } from '../lib/utils/query';
 
-export class ErrorMsg extends HTMLElement {
-  connectedCallback() {
-    if (!this.shadowRoot) {
-      const shadow = this.attachShadow({ mode: 'open' });
-      shadow.adoptedStyleSheets = STYLES;
-      shadow.appendChild(this.renderComponent());
+const COMP_FRAG = createFragment(/* html */ html`
+  <div class="is-column is-gap is-border is-padding">
+    <div class="is-error is-text-bold is-text-larg">Error</div>
+    <p id="error-msg"></p>
+    <div class="is-row is-gap">
+      <button id="error-btn" class="btn" type="button">Ok</button>
+    </div>
+  </div>
+`);
 
-      document.addEventListener('error-msg', this.onError.bind(this));
-      this.style.display = 'none';
+/**
+ * The class displays an error message. The message is displayed when a custom
+ * event is triggered and shows the message of the event. The message has to be
+ * manually confirmed to disappear.
+ */
+export class ErrorMsg extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.adoptedStyleSheets = STYLES;
+    shadow.appendChild(COMP_FRAG.cloneNode(true));
+
+    this.style.display = 'none';
+  }
+
+  connectedCallback() {
+    if (this.shadowRoot !== null) {
+      $<HTMLElement>('#error-btn', this.shadowRoot).onclick = this.onOk;
+      document.addEventListener('error-msg', this.onError);
     }
   }
 
-  onError(e: Event) {
+  disconnectedCallback() {
+    if (this.shadowRoot != null) {
+      $<HTMLElement>('#error-btn', this.shadowRoot).onclick = null;
+      document.removeEventListener('error-msg', this.onError);
+    }
+  }
+
+  onError = (e: Event) => {
     if (this.shadowRoot) {
       const detail = (e as CustomEvent).detail;
       $('#error-msg', this.shadowRoot).textContent = detail;
       this.style.display = 'block';
     }
-  }
+  };
 
-  onOk() {
+  onOk = () => {
     if (this.shadowRoot) {
       $('#error-msg', this.shadowRoot).textContent = '';
       this.style.display = 'none';
     }
-  }
-
-  renderComponent() {
-    const str = /* html */ html`
-      <div class="is-column is-gap is-border is-padding">
-        <div class="is-error is-text-bold is-text-larg">Error</div>
-        <p id="error-msg"></p>
-        <div class="is-row is-gap">
-          <button id="error-btn" class="btn" type="button">Ok</button>
-        </div>
-      </div>
-    `;
-    const frag = createFragment(str);
-
-    $<HTMLElement>('#error-btn', frag).onclick = this.onOk.bind(this);
-
-    return frag;
-  }
+  };
 }
