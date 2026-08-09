18c78c20f2a4a7e7b69b531bfdade4265aef6c2a
Dead End <dead.end@inbox.ru>
2026-08-09 09:22:02 +0200

    update

--- src/components/LessionContinue.ts
+++ src/components/LessionContinue.ts
@@ -5,18 +5,45 @@ import { lessionExists, lessionRemove } from '../lib/model/lession';
 import { STYLES } from '../lib/ui/stylesheets';
 import { $ } from '../lib/utils/query';
 
-export class LessionContinue extends HTMLElement {
-  connectedCallback() {
-    if (!this.shadowRoot) {
-      const shadow = this.attachShadow({ mode: 'open' });
-      shadow.adoptedStyleSheets = STYLES;
-      shadow.appendChild(this.renderComponent());
-    }
+const COMP_FRAG = createFragment(/* html */ html`
+  <div class="is-row is-gap">
+    <button class="btn" id="btn-continue">Continue</button>
+    <button class="btn" id="btn-end">End</button>
+  </div>
+`);
 
-    this.render();
+/**
+ * The class renders the lession continue and cancel buttons on the homepage.
+ * It checks if a lession exists.If so, it provides a button to end the lession
+ * and one with a redirect to continue the lession.
+ */
+export class LessionContinue extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.adoptedStyleSheets = STYLES;
+    shadow.appendChild(COMP_FRAG.cloneNode(true));
   }
 
-  render() {
+  connectedCallback() {
+    if (this.shadowRoot != null) {
+      $<HTMLButtonElement>('#btn-continue', this.shadowRoot).onclick =
+        this.onContinue;
+      $<HTMLButtonElement>('#btn-end', this.shadowRoot).onclick = this.onEnd;
+    }
+
+    this.checkLessons();
+  }
+
+  disconnectedCallback() {
+    if (this.shadowRoot != null) {
+      $<HTMLButtonElement>('#btn-continue', this.shadowRoot).onclick = null;
+
+      $<HTMLButtonElement>('#btn-end', this.shadowRoot).onclick = null;
+    }
+  }
+
+  checkLessons() {
     if (lessionExists()) {
       this.style.display = 'block';
     } else {
@@ -24,28 +51,12 @@ export class LessionContinue extends HTMLElement {
     }
   }
 
-  onContinue() {
+  onContinue = () => {
     window.location.hash = hashLessionProcess();
-  }
+  };
 
-  onEnd() {
+  onEnd = () => {
     lessionRemove();
     this.style.display = 'none';
-  }
-
-  renderComponent() {
-    const str = /* html */ html`
-      <div class="is-row is-gap">
-        <button class="btn" id="btn-continue">Continue</button>
-        <button class="btn" id="btn-end">End</button>
-      </div>
-    `;
-    const frag = createFragment(str);
-
-    $<HTMLButtonElement>('#btn-continue', frag).onclick =
-      this.onContinue.bind(this);
-    $<HTMLButtonElement>('#btn-end', frag).onclick = this.onEnd.bind(this);
-
-    return frag;
-  }
+  };
 }
